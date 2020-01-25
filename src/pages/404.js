c134dfae79accf26e6d3c2a607c4197d6aed19dd
Ajay Ramachandran <dev@ajay.app>
2020-01-25 13:13:07 -0500

    Made 404 page boring

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,8 +7,7 @@ const NotFoundPage = () => (
     <Layout>
         <SEO title="404: Not found" />
         <div className="container">
-            <h1>NOT FOUND</h1>
-            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+            <h1>Not Found</h1>
         </div>
     </Layout>
 );
